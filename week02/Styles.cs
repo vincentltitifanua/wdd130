6fabe240a8161f6cc67ae62403be44a4f27b90fd
vincetitifanua <156245277+vincetitifanua@users.noreply.github.com>
2025-09-20 06:42:16 +1200

    Week 2 complete

---
+++ week02/Styles.cs
@@ -0,0 +1,40 @@
+/* limit main width */
+main {
+  max-width: 25%;
+  margin: 0 auto;
+}
+
+/* only affect profile image inside main */
+main img {
+  width: 50px;
+  height: 60px;
+}
+
+/* aside styles */
+aside {
+  width: 20px;
+  margin: 20px auto;
+  border: 20px solid #ccc;
+  padding: 10px;
+  background-color: #f0f8ff;
+  color: #000033;
+  text-align: center;
+}
+
+aside img {
+  width: 20px;
+  height: auto; /* keeps image ratio */
+}
+
+/* paragraphs */
+p {
+  padding: 0;
+}
+
+/* footer styles */
+footer {
+  margin-top: 2px;
+  border-top: 2px solid #ccc;
+  text-align: center;
+  padding-top: 10px;
+}
